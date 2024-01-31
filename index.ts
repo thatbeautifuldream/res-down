fd156333b44d02f280d3d890f2f0bf9cc8c87c8f
Milind Mishra <milind.mishra4@gmail.com>
2024-01-31 12:34:30 +0530

    [NEW] add logs

--- index.ts
+++ index.ts
@@ -4,18 +4,30 @@ import puppeteer from "puppeteer";
 type PrintPDF = { url: string };
 
 async function printPDF({ url }: PrintPDF) {
-  const browser = await puppeteer.launch({ headless: true });
+  console.log(`Launching Puppeteer and navigating to: ${url}`);
+  const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
+
+  console.log(`Opening page and waiting for network idle`);
   await page.goto(url, { waitUntil: "networkidle0" });
+
+  console.log(`Generating PDF with A4 format`);
   const pdf = await page.pdf({ format: "A4" });
 
+  console.log(`Closing Puppeteer browser`);
   await browser.close();
+
+  console.log(`PDF generation completed`);
   return pdf;
 }
 
+const outputFilePath = "milind-mishra.pdf";
+
 fs.writeFileSync(
-  "milind-mishra.pdf",
+  outputFilePath,
   await printPDF({
     url: "https://docs.google.com/document/d/1ujYf9MIOEeH3UpaHKZmXm_9-W6XpSQ5LAcLUv0ue32k/preview",
   })
 );
+
+console.log(`PDF saved to: ${__dirname}/${outputFilePath}`);
