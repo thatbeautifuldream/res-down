e49f01d7b1143889e903ac7e49f0f09e0583ec4d
Milind Mishra <milind.mishra4@gmail.com>
2024-02-02 15:02:35 +0530

    [NEW] upload to s3 and get presigned url from shell

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { $ } from "bun";
 import fs from "fs";
 import dotenv from "dotenv";
 import puppeteer from "puppeteer";
@@ -10,17 +11,12 @@ async function printPDF({ url }: PrintPDF) {
   console.log(`Launching Puppeteer and navigating to: ${url}`);
   const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
-
   console.log(`Opening page and waiting for network idle`);
   await page.goto(url, { waitUntil: "networkidle0" });
-
   console.log(`Generating PDF with A4 format`);
   const pdf = await page.pdf({ format: "A4" });
-
-  console.log(`Closing Puppeteer browser`);
-  await browser.close();
-
   console.log(`PDF generation completed`);
+  await browser.close();
   return pdf;
 }
 
@@ -31,11 +27,21 @@ if (fs.existsSync(outputFilePath)) {
   fs.unlinkSync(outputFilePath); // delete file
 }
 
-fs.writeFileSync(
-  outputFilePath,
-  await printPDF({
-    url: process.env.GOOGLE_DOCS_PREVIEW_URL as string,
-  })
-);
+const pdf = await printPDF({
+  url: process.env.GOOGLE_DOCS_PREVIEW_URL as string,
+});
+
+fs.writeFileSync(outputFilePath, pdf);
 
 console.log(`PDF saved to: ${__dirname}/${outputFilePath}`);
+
+const s3BucketName = process.env.AWS_S3_BUCKET_NAME as string;
+
+// aws s3 operations
+(async () => {
+  console.log(`Uploading PDF to S3`);
+  await $`aws s3 rm s3://${s3BucketName}/${outputFilePath}`;
+  await $`aws s3 cp ./${outputFilePath} s3://${s3BucketName}/${outputFilePath}`;
+  await $`aws s3 presign s3://${s3BucketName}/${outputFilePath}`;
+  console.log(`PDF uploaded to S3`);
+})();
